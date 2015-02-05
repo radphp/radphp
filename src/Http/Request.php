9eee391e986ce518c8e0f96169d03a7ebc8dd152
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-02-05 12:28:59 +0330

    Add Http\Request class

---
+++ src/Http/Request.php
@@ -0,0 +1,489 @@
+<?php
+
+namespace Rad\Http;
+
+/**
+ * Http Request
+ *
+ * @package Rad\Http
+ */
+class Request
+{
+    /**
+     * Which request method was used to access the page; i.e. 'GET', 'HEAD', 'POST', 'PUT'.
+     *
+     * @var string
+     */
+    protected $method;
+    protected $uri;
+
+    /**
+     * Contents of the User-Agent: header from the current request,
+     * if there is one. This is a string denoting the user agent being which is accessing the page.
+     * A typical example is: Mozilla/4.5 [en] (X11; U; Linux 2.2.9 i586).
+     *
+     * @var string
+     */
+    protected $userAgent;
+    protected $serverAddress;
+    protected $serverName;
+    protected $httpHost;
+    protected $contentType;
+    protected $input;
+    protected $body;
+    protected $jsonBody;
+    private $superGlobals = [];
+
+    /**
+     * Whether or not to trust HTTP_X headers set by most load balancers.
+     * Only set to true if your application runs behind load balancers/proxies
+     * that you control.
+     *
+     * @var bool
+     */
+    public $trustProxy = false;
+
+    const METHOD_POST = 'POST';
+    const METHOD_GET = 'GET';
+    const METHOD_PUT = 'PUT';
+    const METHOD_PATCH = 'PATCH';
+    const METHOD_HEAD = 'HEAD';
+    const METHOD_DELETE = 'DELETE';
+    const METHOD_OPTIONS = 'OPTIONS';
+
+    /**
+     * Http request constructor
+     */
+    public function __construct()
+    {
+        $this->superGlobals = [
+            'post' => $_POST,
+            'query' => $_GET,
+            'request' => $_REQUEST,
+            'server' => $_SERVER,
+            'environment' => $_ENV,
+            'files' => $_FILES
+        ];
+
+        $this->method = $this->superGlobals['server']['REQUEST_METHOD'];
+        $this->contentType = $this->superGlobals['server']['CONTENT_TYPE'];
+        $this->userAgent = $this->superGlobals['server']['HTTP_USER_AGENT'];
+        $this->serverAddress = $this->superGlobals['server']['SERVER_ADDR'];
+        $this->serverName = $this->superGlobals['server']['SERVER_NAME'];
+        $this->httpHost = $this->superGlobals['server']['HTTP_HOST'];
+        $this->input = $this->prepareInput();
+    }
+
+    /**
+     * Gets a variable from the $_REQUEST super global.
+     * If no parameters are given the $_REQUEST super global is returned
+     *
+     * @param string|null $name
+     * @param mixed       $defaultValue
+     *
+     * @return mixed
+     */
+    public function get($name = null, $defaultValue = null)
+    {
+        if (array_key_exists($name, $this->superGlobals['request'])) {
+            return $this->superGlobals['request'][$name];
+        } elseif ($defaultValue) {
+            return $defaultValue;
+        }
+
+        return $this->superGlobals['request'];
+    }
+
+    /**
+     * Gets a variable from the $_POST super global.
+     * If no parameters are given the $_POST super global is returned
+     *
+     * @param string|null $name
+     * @param mixed       $defaultValue
+     *
+     * @return mixed
+     */
+    public function getPost($name = null, $defaultValue = null)
+    {
+        if (array_key_exists($name, $this->superGlobals['post'])) {
+            return $this->superGlobals['post'][$name];
+        } elseif ($defaultValue) {
+            return $defaultValue;
+        }
+
+        return $this->superGlobals['post'];
+    }
+
+    /**
+     * Gets a variable from put request
+     *
+     * @param string|null $name
+     * @param mixed       $defaultValue
+     *
+     * @return mixed
+     */
+    public function getPut($name = null, $defaultValue = null)
+    {
+        if (array_key_exists($name, $this->input)) {
+            return $this->input[$name];
+        } elseif ($defaultValue) {
+            return $defaultValue;
+        }
+
+        return $this->input;
+    }
+
+    /**
+     * Gets variable from $_GET super global.
+     * If no parameters are given the $_GET super global is returned
+     *
+     * @param string|null $name
+     * @param mixed       $defaultValue
+     *
+     * @return mixed
+     */
+    public function getQuery($name = null, $defaultValue = null)
+    {
+        if (array_key_exists($name, $this->superGlobals['query'])) {
+            return $this->superGlobals['query'][$name];
+        } elseif ($defaultValue) {
+            return $defaultValue;
+        }
+
+        return $this->superGlobals['query'];
+    }
+
+    /**
+     * Gets variable from $_SERVER super global
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function getServer($name)
+    {
+        if (array_key_exists($name, $this->superGlobals['server'])) {
+            return $this->superGlobals['server'][$name];
+        }
+    }
+
+    /**
+     * Checks whether $_REQUEST super global has certain index
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function has($name)
+    {
+        return array_key_exists($name, $this->superGlobals['request']);
+    }
+
+    /**
+     * Checks whether $_POST super global has certain index
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasPost($name)
+    {
+        return array_key_exists($name, $this->superGlobals['post']);
+    }
+
+    /**
+     * Checks whether put has certain index
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasPut($name)
+    {
+        return array_key_exists($name, $this->input);
+    }
+
+    /**
+     * Checks whether $_GET super global has certain index
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasQuery($name)
+    {
+        return array_key_exists($name, $this->superGlobals['query']);
+    }
+
+    /**
+     * Checks whether $_SERVER super global has certain index
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasServer($name)
+    {
+        return array_key_exists($name, $this->superGlobals['server']);
+    }
+
+    /**
+     * Gets HTTP schema (http/https)
+     *
+     * @return string
+     */
+    public function getScheme()
+    {
+        if ($this->trustProxy && getenv('HTTP_X_FORWARDED_PROTO')) {
+            return getenv('HTTP_X_FORWARDED_PROTO');
+        }
+
+        return getenv('HTTPS') ? 'https' : 'http';
+    }
+
+    /**
+     * Checks whether request has been made using ajax.
+     * Checks if $_SERVER[‘HTTP_X_REQUESTED_WITH’]==’XMLHttpRequest’
+     *
+     * @return bool
+     */
+    public function isAjax()
+    {
+        return (getenv('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest');
+    }
+
+    /**
+     * Checks whether request has been made using any secure layer
+     *
+     * @return bool
+     */
+    public function isSecureRequest()
+    {
+        return ($this->getScheme() == 'https');
+    }
+
+    /**
+     * Gets HTTP raw request body
+     *
+     * @return string
+     */
+    public function getRawBody()
+    {
+        if ($this->body) {
+            return $this->body;
+        }
+
+        $fh = fopen(STDIN, 'r');
+        $this->body = stream_get_contents($fh);
+        fclose($fh);
+
+        return $this->body;
+    }
+
+    /**
+     * Gets decoded JSON HTTP raw request body
+     *
+     * @return array|mixed
+     */
+    public function getJsonRawBody()
+    {
+        if ($this->jsonBody) {
+            return $this->jsonBody;
+        }
+
+        if ($this->getContentType() == 'application/json') {
+            return $this->jsonBody = json_decode($this->getRawBody());
+        } else {
+            return $this->jsonBody = '';
+        }
+    }
+
+    /**
+     * Get request content type
+     *
+     * @return string
+     */
+    public function getContentType()
+    {
+        return $this->contentType;
+    }
+
+    /**
+     * Gets active server address IP
+     *
+     * @return string
+     */
+    public function getServerAddress()
+    {
+        return $this->serverAddress;
+    }
+
+    /**
+     * Gets active server name
+     *
+     * @return string
+     */
+    public function getServerName()
+    {
+        return $this->serverName;
+    }
+
+    /**
+     * Gets information about schema, host and port used by the request
+     *
+     * @return string
+     */
+    public function getHttpHost()
+    {
+        return $this->httpHost;
+    }
+
+    /**
+     * Gets most possible client IPv4 Address.
+     * This method search in $_SERVER[‘REMOTE_ADDR’] and optionally in $_SERVER[‘HTTP_X_FORWARDED_FOR’]
+     *
+     * @return string
+     */
+    public function getClientAddress()
+    {
+        if ($this->trustProxy && getenv('HTTP_X_FORWARDED_FOR')) {
+            return getenv('HTTP_X_FORWARDED_FOR');
+        }
+
+        return getenv('REMOTE_ADDR');
+    }
+
+    /**
+     * Gets HTTP method which request has been made
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Gets HTTP URI which request has been made
+     *
+     * @return string
+     */
+    public function getURI()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Gets HTTP user agent used to made the request
+     *
+     * @return string
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * Check if HTTP method match any of the passed methods
+     *
+     * @param string|array $methods
+     *
+     * @return bool
+     */
+    public function isMethod($methods)
+    {
+        if (is_array($methods)) {
+            return in_array($this->getMethod(), $methods);
+        }
+
+        return ($this->getMethod() == $methods);
+    }
+
+    /**
+     * Checks whether HTTP method is POST.
+     *
+     * @return bool
+     */
+    public function isPost()
+    {
+        return ($this->getMethod() == self::METHOD_POST);
+    }
+
+    /**
+     * Checks whether HTTP method is GET.
+     *
+     * @return bool
+     */
+    public function isGet()
+    {
+        return ($this->getMethod() == self::METHOD_GET);
+    }
+
+    /**
+     * Checks whether HTTP method is PUT.
+     *
+     * @return bool
+     */
+    public function isPut()
+    {
+        return ($this->getMethod() == self::METHOD_PUT);
+    }
+
+    /**
+     * Checks whether HTTP method is PATCH.
+     *
+     * @return bool
+     */
+    public function isPatch()
+    {
+        return ($this->getMethod() == self::METHOD_PATCH);
+    }
+
+    /**
+     * Checks whether HTTP method is HEAD.
+     *
+     * @return bool
+     */
+    public function isHead()
+    {
+        return ($this->getMethod() == self::METHOD_HEAD);
+    }
+
+    /**
+     * Checks whether HTTP method is DELETE.
+     *
+     * @return bool
+     */
+    public function isDelete()
+    {
+        return ($this->getMethod() == self::METHOD_DELETE);
+    }
+
+    /**
+     * Checks whether HTTP method is OPTIONS.
+     *
+     * @return bool
+     */
+    public function isOptions()
+    {
+        return ($this->getMethod() == self::METHOD_OPTIONS);
+    }
+
+    /**
+     * Prepare input
+     *
+     * @return mixed|string
+     */
+    protected function prepareInput()
+    {
+        $content = $this->getRawBody();
+
+        if ($this->getContentType() == 'application/json') {
+            $content = json_decode($content);
+        } elseif ($this->getContentType() == 'application/x-www-form-urlencoded') {
+            parse_str($content, $content);
+        }
+
+        return $content;
+    }
+}
