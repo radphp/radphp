1d0c3f35679c39d2c5af3f8818ef4299a36bddbd
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-03-14 01:45:54 +0330

    Add Singleton trait

---
+++ src/Core/Pattern/SingletonTrait.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Rad\Core\Pattern;
+
+/**
+ * Singleton trait
+ *
+ * @package Rad\Core\Pattern
+ */
+trait SingletonTrait
+{
+    protected static $instance;
+
+    /**
+     * Get instance
+     *
+     * @return static
+     */
+    final public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new static;
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Constructor
+     */
+    final private function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * Init method for call in constructor
+     */
+    protected function init()
+    {
+    }
+
+    /**
+     * Ignore magic clone
+     */
+    final private function __clone()
+    {
+    }
+
+    /**
+     * Ignore sleep magic method
+     */
+    final private function __sleep()
+    {
+    }
+
+    /**
+     * Ignore wakeup magic method
+     */
+    final private function __wakeup()
+    {
+    }
+}
