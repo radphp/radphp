06555c4b96f3bc053c6b7d98f8a03b8adf21917d
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-04-02 00:21:41 +0430

    Add NotFoundException

---
+++ src/Http/Exception/NotFoundException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rad\Http\Exception;
+
+use Rad\Exception;
+
+/**
+ * NotFoundException
+ *
+ * @package Rad\Http\Exception
+ */
+class NotFoundException extends Exception
+{
+    /**
+     * NotFoundException constructor
+     *
+     * @param string     $message
+     * @param int        $code
+     * @param \Exception $previous \
+     */
+    public function __construct($message = '', $code = 404, \Exception $previous = null)
+    {
+        if (empty($message)) {
+            $message = 'Not Found';
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
