a2aed9bf4bb56f273908e3eb9429474841356c10
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-04-01 16:58:34 +0430

    Add constructor

--- src/Exception.php
+++ src/Exception.php
@@ -9,5 +9,15 @@ namespace Rad;
  */
 class Exception extends \Exception
 {
-
+    /**
+     * Rad\Exception constructor
+     *
+     * @param string $message
+     * @param int    $code
+     * @param null   $previous
+     */
+    public function __construct($message, $code = 500, $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
