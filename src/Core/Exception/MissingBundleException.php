de1f6bdb5205ebdee7a69f785efcdd94571ed497
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-04-25 01:12:06 +0430

    Add MissingBundleException

---
+++ src/Core/Exception/MissingBundleException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rad\Core\Exception;
+
+use Rad\Exception;
+
+/**
+ * Missing Bundle Exception
+ *
+ * @package Rad\Core\Exception
+ */
+class MissingBundleException extends Exception
+{
+
+}
