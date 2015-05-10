4403c553ec1d15f134e011465b8145309739efe0
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-05-10 23:53:09 +0430

    implement EventSubscriberInterface

--- src/Core/Action.php
+++ src/Core/Action.php
@@ -2,9 +2,12 @@
 
 namespace Rad\Core;
 
+use Rad\Application;
 use Rad\DependencyInjection\ContainerAware;
 use Rad\Error\ErrorHandler;
-use Rad\Event\EventDispatcher;
+use Rad\Events\Event;
+use Rad\Events\EventManager;
+use Rad\Events\EventSubscriberInterface;
 use Rad\Network\Http\Request;
 use Rad\Network\Http\Response;
 use Rad\Network\Http\Response\Cookies;
@@ -22,16 +25,16 @@ use Rad\Routing\Router;
  * @property Responder    $responder
  * @property ErrorHandler $error
  *
- * @method Request         getRequest()  Get Http request
- * @method Response        getResponse() Get Http response
- * @method Router          getRouter()   Get router
- * @method Cookies         getCookies()  Get cookies
- * @method EventDispatcher getEvent()    Get event dispatcher
- * @method ErrorHandler    getError()    Get error handler
+ * @method Request         getRequest()      Get Http request
+ * @method Response        getResponse()     Get Http response
+ * @method Router          getRouter()       Get router
+ * @method Cookies         getCookies()      Get cookies
+ * @method EventManager    getEventManager() Get event dispatcher
+ * @method ErrorHandler    getError()        Get error handler
  *
  * @package Rad\Core
  */
-abstract class Action extends ContainerAware
+abstract class Action extends ContainerAware implements EventSubscriberInterface
 {
     protected $responder;
 
@@ -43,5 +46,60 @@ abstract class Action extends ContainerAware
     public function __construct($responder)
     {
         $this->responder = $responder;
+    }
+
+    /**
+     * Call before web method
+     *
+     * @param Event $event
+     */
+    public function beforeWebMethod(Event $event)
+    {
+
+    }
+
+    /**
+     * Call after web method
+     *
+     * @param Event $event
+     */
+    public function afterWebMethod(Event $event)
+    {
+
+    }
+
+    /**
+     * Call before cli method
+     *
+     * @param Event $event
+     */
+    public function beforeCliMethod(Event $event)
+    {
+
+    }
+
+    /**
+     * Call after cli method
+     *
+     * @param Event $event
+     */
+    public function afterCliMethod(Event $event)
+    {
+
+    }
+
+    /**
+     * Subscribe event listener
+     *
+     * @param EventManager $eventManager
+     *
+     * @return mixed
+     */
+    public function subscribe(EventManager $eventManager)
+    {
+        $eventManager->attach(Application::EVENT_BEFORE_WEB_METHOD, [$this, 'beforeWebMethod'])
+            ->attach(Application::EVENT_AFTER_WEB_METHOD, [$this, 'afterWebMethod'])
+            ->attach(Application::EVENT_BEFORE_CLI_METHOD, [$this, 'beforeCLiMethod'])
+            ->attach(Application::EVENT_AFTER_CLI_METHOD, [$this, 'afterCLiMethod']);
     }
 }
