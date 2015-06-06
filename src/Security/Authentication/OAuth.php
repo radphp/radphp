ff90597173fafe4ff0ca84a1756799713032b9eb
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-06 23:52:35 +0430

    Added OAuth class

---
+++ src/Security/Authentication/OAuth.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Rad\Security\Authentication;
+
+use Rad\Security\Authentication\OAuth\Config;
+use Rad\Security\Authentication\OAuth\ConfigManager;
+
+/**
+ * OAuth
+ *
+ * @package Rad\Security\Authentication
+ */
+class OAuth
+{
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * Rad\Security\Authentication\OAuth constructor
+     *
+     * @param string $configId
+     *
+     * @throws Exception
+     */
+    public function __construct($configId)
+    {
+        if (ConfigManager::exist($configId)) {
+            $this->config = ConfigManager::get($configId);
+        } else {
+            throw new Exception(sprintf('OAuth config id "%s" does not exist.', $configId));
+        }
+    }
+
+    /**
+     * Get authorize uri
+     *
+     * @return string
+     */
+    public function getAuthorizeUri()
+    {
+        return $this->config->getProvider()->getAuthorizeUri();
+    }
+
+    /**
+     * Get access token
+     *
+     * @return string
+     */
+    public function getAccessToken()
+    {
+        return $this->config->getProvider()->getAccessToken();
+    }
+
+    /**
+     * Get user
+     *
+     * @param string $token Access token
+     *
+     * @return OAuth\User
+     */
+    public function getUser($token)
+    {
+        return $this->config->getProvider()->getUser($token);
+    }
+}
