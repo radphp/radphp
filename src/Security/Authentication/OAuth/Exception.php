73743171fa1e8a75070573a65e5634cfac32e2dd
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-08 23:50:43 +0430

    Added OAuth Exception

---
+++ src/Security/Authentication/OAuth/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rad\Security\Authentication\OAuth;
+
+/**
+ * OAuth Exception
+ *
+ * @package Rad\Security\Authentication\OAuth
+ */
+class Exception extends \Rad\Exception
+{
+
+}
