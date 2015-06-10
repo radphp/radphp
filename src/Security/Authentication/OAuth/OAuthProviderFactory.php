e2de91c094e5d1d1e3f3a68c7aace6ecf1ae92fc
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-10 23:47:05 +0430

    Added OAuth Provider Factory

---
+++ src/Security/Authentication/OAuth/OAuthProviderFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Rad\Security\Authentication\OAuth;
+
+use Rad\Utility\Inflection;
+
+/**
+ * OAuth Provider Factory
+ *
+ * @package Rad\Security\Authentication\OAuth
+ */
+class OAuthProviderFactory
+{
+    /**
+     * Create OAuth provider
+     *
+     * @param string $oAuthProvider
+     *
+     * @return AbstractOAuthProvider
+     * @throws Exception
+     */
+    public static function create($oAuthProvider)
+    {
+        if (strpos($oAuthProvider, '\\') !== false) {
+            if (class_exists($oAuthProvider)) {
+                $providerInstance = new $oAuthProvider();
+                if (!($providerInstance instanceof AbstractOAuthProvider)) {
+                    throw new Exception(
+                        sprintf(
+                            'OAuth provider "%s" must be extend "AbstractOAuthProvider".',
+                            $oAuthProvider
+                        )
+                    );
+                }
+
+                return $providerInstance;
+            } else {
+                throw new Exception(sprintf('OAuth provider "%s" does not exist.', $oAuthProvider));
+            }
+        }
+
+        $oAuthClass = 'Rad\\Security\\Authentication\\OAuth\\Provider\\' . Inflection::camelize($oAuthProvider) . 'Provider';
+
+        if (class_exists($oAuthClass)) {
+            return new $oAuthClass();
+        } else {
+            throw new Exception(sprintf('OAuth provider "%s" does not exist.', $oAuthClass));
+        }
+    }
+}
