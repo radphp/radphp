3e78cf4c63057603eeaf89b396cb0dac3c73721e
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-06-16 23:46:09 +0430

    Added Abstract OAuth Provider

---
+++ src/Security/Authentication/OAuth/AbstractOAuthProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Rad\Security\Authentication\OAuth;
+
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Abstract OAuth Provider
+ *
+ * @package Rad\Security\Authentication\OAuth
+ */
+abstract class AbstractOAuthProvider
+{
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    /**
+     * Set config
+     *
+     * @param Config $config
+     */
+    public function setConfig(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Get config
+     *
+     * @return Config
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    /**
+     * Config http request
+     *
+     * @param RequestInterface $request Http request
+     *
+     * @return RequestInterface
+     */
+    protected function request(RequestInterface $request)
+    {
+        return $request->withHeader('Accept', 'application/json')
+            ->withHeader('User-Agent', 'RadPHP-OAuth');
+    }
+
+    /**
+     * Get authorize uri
+     *
+     * @return string
+     */
+    abstract public function getAuthorizeUri();
+
+    /**
+     * Get access token
+     *
+     * @return string
+     * @throws Exception
+     */
+    abstract public function getAccessToken();
+
+    /**
+     * Get user detail
+     *
+     * @param string $token Access token
+     *
+     * @return User
+     * @throws Exception
+     */
+    abstract public function getUser($token);
+}
