30167d4d400ae231582362af4da1994640525732
Mohammad Abdoli Rad <m.abdolirad@gmail.com>
2015-08-16 22:27:48 +0430

    Added Session Exception

---
+++ src/Network/Session/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rad\Network\Session;
+
+use Rad\Core\Exception\BaseException;
+
+/**
+ * Session Exception
+ *
+ * @package Rad\Network\Session
+ */
+class Exception extends BaseException
+{
+
+}
